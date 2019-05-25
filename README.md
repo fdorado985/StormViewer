ff7894c8d209b6e77f73ea5e8874a7e936420db6
Juan Francisco Dorado Torres <fdorado985@gmail.com>
2019-05-25 02:27:14 -0500

    Load the images and add them to a pictures property

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 ## Description
 You'll see an app that lets users scroll through a list of images, then select one to view.
 
+## Programming To-Do List
+- [x] **Listing Images with FileManager** : Get the images from the directory inside our app.
+- [ ] **Collect the NSSL images** : Look for the images with the prefix `nssl` and add them to an array property.
+
 ## Demo
+### features/listing-images-with-filemanager
+| Screenshot 01 |
+| ------------- |
+| ![screenshot01](.screenshots/screenshot01.png) |
--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: - Public Properties
+
+  var pictures = [String]()
+
+  // MARK: - View cycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    let fm = FileManager.default // This let us work with the filesystem that we will be using to look for files.
+    let path = Bundle.main.resourcePath! // This sets the resource path of our app's bundle. "Tell me where I can find all those images I added to my app."
+    let items = try! fm.contentsOfDirectory(atPath: path) // This give us a collection of all the contents of the directory at a path.
+
+    // Iterate through each item in our collection
+    for item in items {
+      if item.hasPrefix("nssl") { // Look just for those who starts with 'nssl' - our images.
+        pictures.append(item) // Add the item to our pictures property
+      }
+    }
+
+    print(pictures)
   }
-
-
 }
-
